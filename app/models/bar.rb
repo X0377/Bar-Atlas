7eed499ed3ef26045fd4469a3c4da2e4f6e1010b
X0377 <mutoh0377@gmail.com>
2025-06-19 02:27:23 +0900

    Improve UI layout, add sample bar data

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,9 +1,8 @@
 class Bar < ApplicationRecord
   has_many :specialties, dependent: :destroy
 
-  # Ransack検索許可設定
   def self.ransackable_attributes(auth_object = nil)
-    ["name", "address", "price_range", "smoking_status", "description", "business_hours", "created_at", "updated_at"]
+    ["name", "address", "phone", "price_range", "smoking_status", "description", "business_hours", "created_at", "updated_at"]
   end
 
   def self.ransackable_associations(auth_object = nil)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_08_043753) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_18_171254) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_08_043753) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone"
+    t.string "business_hours"
   end
 
   create_table "specialties", force: :cascade do |t|
