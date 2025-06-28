f4345ab3045d0efb6069bb5caa8eabf2e355af1f
X0377 <mutoh0377@gmail.com>
2025-06-28 19:41:34 +0900

    importmapsの設定不備を解消、showの簡易実装

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
-import "./search_filters";
+import "search_filters";
 
 console.log("Application JavaScript loaded");
